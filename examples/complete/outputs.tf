fc0ea4e5ba9ef9e454899861cf183274321de527
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2026-09-16 00:48:49 +0530

    add detailed description

--- examples/complete/outputs.tf
+++ examples/complete/outputs.tf
@@ -1,11 +1,14 @@
 output "vpc" {
-  value = module.myvpc.vpc_id
+  description = "The unique AWS Resource ID of the Virtual Private Cloud (VPC) created by the myvpc module."
+  value       = module.myvpc.vpc_id
 }
 
 output "public_subnet" {
-  value = module.myvpc.public_subnet
+  description = "List of IDs for the public subnets that have routes to an Internet Gateway."
+  value       = module.myvpc.public_subnet
 }
 
 output "private_subnet" {
-  value = module.myvpc.private_subnet
+  description = "List of IDs for the isolated private subnets reserved for internal resources."
+  value       = module.myvpc.private_subnet
 }
